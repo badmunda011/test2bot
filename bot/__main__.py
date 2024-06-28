c38894788048ba88bf5eb25d886f95d8677f5ef8
Aman Gautam <129865249+amangautamdev@users.noreply.github.com>
2024-06-28 13:31:24 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -58,10 +58,10 @@ async def start(_, message):
     elif config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Bot Started.\n' \
                        'Now I will send all of your stuffs here.\n' \
-                       'Use me at: @Public_Leech_DL'
+                       'Use me at: @PublicLeechDL'
     elif not config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Sorry, you cannot use me here!\n' \
-                       'Join: @Public_Leech_DL to use me.\n' \
+                       'Join: @PublicLeechDL to use me.\n' \
                        'Thank You'
     else:
         tag = message.from_user.mention
