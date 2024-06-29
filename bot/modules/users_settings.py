3cfe75debfc05a22822cb6f6ca942686bc02c1c7
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-06-29 14:37:56 +0530

    Update users_settings.py

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -397,13 +397,13 @@ Timeout: 60 sec
         rmsg = f'''
 Send Leech Prefix. Timeout: 60 sec
 Examples:
-1. <code>{escape('<b>Join: @Z_Mirror</b>')}</code> 
+1. <code>{escape('<b>Join: @PBX_PERMOT</b>')}</code> 
 This will give output as:
-<b>Join: @Z_Mirror</b>  <code>69MB.bin</code>.
+<b>Join: @PBX_PERMOT</b>  <code>69MB.bin</code>.
 
-2. <code>{escape('<code>Join: @Z_Mirror</code>')}</code> 
+2. <code>{escape('<code>Join: @PBX_PERMOT</code>')}</code> 
 This will give output as:
-<code>Join: @Z_Mirror</code> <code>69MB.bin</code>.
+<code>Join: @PBX_PERMOT</code> <code>69MB.bin</code>.
 
 Check all available formatting options <a href="https://core.telegram.org/bots/api#formatting-options">HERE</a>.
         '''
